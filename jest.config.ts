4ba2e01c6c8268d471df4acafc43c4dcd36ff258
KyleTryon <git@techsquidtv.com>
2023-04-18 15:10:37 -0400

    feat!: replace jest with vitest

--- jest.config.ts
+++
@@ -1,8 +0,0 @@
-import type { JestConfigWithTsJest } from "ts-jest";
-
-const jestConfig: JestConfigWithTsJest = {
-  preset: "ts-jest/presets/js-with-babel-esm",
-  extensionsToTreatAsEsm: [".ts"],
-};
-
-export default jestConfig;
